9c3a57ae85cf507c51e1d64274319c9fbdc6a91b
unknown <maged.ghabour2016@gmail.com>
2026-04-22 16:56:19 +0200

    footer

--- single-service.php
+++ single-service.php
@@ -12,10 +12,21 @@ get_header();
 <main id="primary" class="site-main service-single-main" role="main">
 
 	<!-- Service Hero -->
+	<?php
+	// Dynamic background image: Check ACF 'service_cover' first, then Featured Image, then fallback
+	$bg_url = '';
+	if ( function_exists('get_field') && get_field('service_cover') ) {
+		$bg_url = esc_url( get_field('service_cover')['url'] );
+	} elseif ( has_post_thumbnail() ) {
+		$bg_url = esc_url( get_the_post_thumbnail_url( null, 'full' ) );
+	} else {
+		$bg_url = esc_url( get_theme_mod( 'hero_default_bg', AMAL_ASSETS . '/images/hero-bg.jpg' ) );
+	}
+	?>
 	<div class="service-hero">
 		<div class="service-hero__overlay" aria-hidden="true"></div>
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="service-hero__bg" style="background-image: url('<?php echo esc_url( get_the_post_thumbnail_url( null, 'hero-bg' ) ); ?>')"></div>
+		<?php if ( $bg_url ) : ?>
+			<div class="service-hero__bg" style="background-image: url('<?php echo $bg_url; ?>')"></div>
 		<?php endif; ?>
 		<div class="container service-hero__content">
 			<h1 class="service-hero__title"><?php the_title(); ?></h1>
