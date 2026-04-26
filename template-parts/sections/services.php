873963e0616b5de3cd62af3f40392317fdb653a4
unknown <maged.ghabour2016@gmail.com>
2026-04-26 11:29:13 +0300

    services

--- template-parts/sections/services.php
+++ template-parts/sections/services.php
@@ -62,8 +62,8 @@ $services = new WP_Query( $args );
 								<?php _e( 'تواصل معنا', 'amal-malki' ); ?>
 								<span class="service-card__arrow" aria-hidden="true">
 									<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-										<line x1="7" y1="7" x2="17" y2="17"/>
-										<polyline points="7 17 7 7 17 7"/>
+										<line x1="17" y1="7" x2="7" y2="17"/>
+										<polyline points="7 7 7 17 17 17"/>
 									</svg>
 								</span>
 							</a>
